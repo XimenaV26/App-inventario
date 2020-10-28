28c6f54ddebc2789258de47a4db14a3b81762b31
XimenaV26 <54459424+XimenaV26@users.noreply.github.com>
2020-10-28 12:33:49 -0600

    avance botones

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var codigo = document.getElementById("codigo")
-var nom = document.getElementById("iptNom")
-var desc = document.getElementById("iptDes")
-var cant = document.getElementById("iptCant")
-var cost = document.getElementById("iptCost")
-var codeB = document.getElementById("iptCodeB")
-var busCod = document.getElementById("iptBuscar")
+var nombre = document.getElementById("nombre")
+var descripcion = document.getElementById("descrip")
+var cantidad = document.getElementById("cantidad")
+var costo = document.getElementById("costo")
+var borrarC = document.getElementById("borrarCode")
+var buscarC = document.getElementById("buscarCode")
 
 import Producto from './producto.js'
 import Inventario from './inventario.js'
